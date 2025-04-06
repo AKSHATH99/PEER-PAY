3fa29c496acdef3ee93948d27cae05c7ac8ddb94
Akshath P <akshathpkk@gmail.com>
2025-04-07 00:21:42 +0530

    moved otp to redis

--- src/app/api/user/auth/verifyOTP/route.js
+++ src/app/api/user/auth/verifyOTP/route.js
@@ -1,15 +1,30 @@
 import { Resend } from "resend";
 import { storeOtp, verifyOtp } from "@/app/lib/otpStore";
 import User from "@/app/model/UserModal";
-import jwt from 'jsonwebtoken';
-import { NextRequest, NextResponse } from 'next/server';
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
 
-export  async function POST(request) {
+export async function POST(request) {
   const { email, otp } = await request.json();
-  const isVerified = verifyOtp(email, otp);
+  const isVerified = await verifyOtp(email, otp);
 
-  return NextResponse.json(
-    { message: "OTP verified" },
-    { status: 200 }
-  );
+
+  try {
+    if (isVerified=="timeout") {
+      return NextResponse.json(
+        { message: "OTP timeout. Resend OTP" },
+        { status: 400 }
+      );
+    }else if(!isVerified){
+      return NextResponse.json(
+        { message: "Some error while otp verification" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json({ message: "OTP verified" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error occured while otp verification" }, { status: 500 });
+  }
 }
